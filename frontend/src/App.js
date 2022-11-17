760ae66dcec5e27aa003d761c4b4f36fa0202528
Shahid321fw11 <shahid_fw11_321@masai.school>
2022-11-17 12:50:24 +0530

    Landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import React, {useState} from "react";
+import axios from 'axios';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      HEllo World
+    <div className="app">
+      <div className="container">
+          <div className="top">
+            <p className="name">Delhi</p>
+            <p className="temp">23 °F</p>
+            <p className="description">Cloudy</p>
+          </div>
+          <div className="bottom">
+            <p className="humidity">Humidity: 29</p>
+            <p className="pressure">Pressure: 1014</p>
+            <p className="wind-speed">Wind-Speed: 3.6MPH</p>
+          </div>
+      </div>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,3 +8,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
+// https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=5a18649b9875dbb4fd25b9dce8346a27
+
