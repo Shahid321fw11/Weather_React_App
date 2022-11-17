5cad6b871e27d8c951624775c774aa9e44a3bdee
Shahid321fw11 <shahid_fw11_321@masai.school>
2022-11-17 17:12:55 +0530

    Front-end of weather app is complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,61 @@
-import React, {useState} from "react";
-import axios from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  const [cityName, setCityName] = useState("");
+  const weather_api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=5a18649b9875dbb4fd25b9dce8346a27`;
+
+  const searchLoaction = (event) => {
+    // console.log(cityName, setCityName);
+    if (event.key === "Enter") {
+      axios.get(weather_api).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setCityName("");
+    }
+  };
+
   return (
     <div className="app">
       <div className="container">
-          <div className="top">
-            <p className="name">Delhi</p>
-            <p className="temp">23 °F</p>
-            <p className="description">Cloudy</p>
-          </div>
-          <div className="bottom">
-            <p className="humidity">Humidity: 29</p>
-            <p className="pressure">Pressure: 1014</p>
-            <p className="wind-speed">Wind-Speed: 3.6MPH</p>
-          </div>
+        <div className="search_box">
+          <input
+            value={cityName}
+            onChange={(event) => setCityName(event.target.value)}
+            onKeyPress={searchLoaction}
+            placeholder="Enter City Name"
+            type="text"
+          ></input>
+        </div>
+
+        <div className="top">
+          <p className="name">{data.name}</p>
+          {data.main ? <p className="temp">{data.main.temp}°F</p> : null}
+          {data.weather ? (
+            <p className="description">{data.weather[0].main}</p>
+          ) : null}
+        </div>
+
+        <div className="bottom">
+          {data.main ? (
+            <p className="humidity">
+              {data.main.humidity}% <br /> Humidity
+            </p>
+          ) : null}
+          {data.main ? (
+            <p className="feelsLike">
+              {data.main.feels_like}°F <br /> Feels Like
+            </p>
+          ) : null}
+          {data.wind ? (
+            <p className="wind_speed">
+              {data.wind.speed} MPH <br /> Wind Speed
+            </p>
+          ) : null}
+        </div>
       </div>
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-
 // https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=5a18649b9875dbb4fd25b9dce8346a27
-
